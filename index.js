e17793a39dd6bc4de50253422b33f3819a6f9676
sadiik1975 <sadiikbeen@gmail.com>
2024-05-08 13:30:18 -0400

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Select the h1 element
-const header = document.querySelector("h1");
+//const header = document.querySelector("h1");
 
 // Check if the element exists and interact with it
-if (header) {
-  console.log("Header before removal:", header.textContent);
-  header.remove();  // Remove the h1 element from the DOM
-  console.log("Header has been removed from the DOM.");
-} else {
-  console.log("No h1 element found!");
-}
+//if (header) {
+ // console.log("Header before removal:", header.textContent);
+  //header.remove();  // Remove the h1 element from the DOM
+ // console.log("Header has been removed from the DOM.");
+//} else {
+ // console.log("No h1 element found!");
+//}
